71bce706806e5929f9765a39de02a0d765e26dce
Perryvw <perryvw@users.noreply.github.com>
2024-06-30 11:17:48 +0200

    Upgrade playground monaco

--- src/pages/play/code.ts
+++ src/pages/play/code.ts
@@ -21,6 +21,32 @@ function onAbilityCast(this: void, caster: Unit, targetLocation: Vector) {
 }
 `;
 
+const exampleOutput = `--[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
+-- Lua Library inline imports
+local function __TS__ArrayFilter(self, callbackfn, thisArg)
+    local result = {}
+    local len = 0
+    for i = 1, #self do
+        if callbackfn(thisArg, self[i], i - 1, self) then
+            len = len + 1
+            result[len] = self[i]
+        end
+    end
+    return result
+end
+-- End of Lua Library inline imports
+function onAbilityCast(caster, targetLocation)
+    local units = findUnitsInRadius(targetLocation, 500)
+    local friends = __TS__ArrayFilter(
+        units,
+        function(____, unit) return caster:isFriend(unit) end
+    )
+    for ____, friend in ipairs(friends) do
+        friend:givePoints(500)
+    end
+end
+`
+
 export function getInitialCode() {
     if (window.location.hash.startsWith("#src=")) {
         const code = window.location.hash.replace("#src=", "").trim();
@@ -33,6 +59,10 @@ export function getInitialCode() {
     }
 
     return example;
+}
+
+export function getInitialLua() {
+    return exampleOutput;
 }
 
 export function updateCodeHistory(code: string) {
